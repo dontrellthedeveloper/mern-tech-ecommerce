d9174745c0272734c84164e158521c11ee382215
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-06-30 23:56:05 -0400

    added react-router-links

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Switch>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+            </Switch>
+        </>
+    );
 };
 
 export default App;
