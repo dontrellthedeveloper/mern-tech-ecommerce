c8e8811aeeebfe02670a5fc8b3fac95792513435
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-01 15:46:19 -0400

    added ability to register to website using firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import RegisterComplete from "./pages/auth/RegisterComplete";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
-import RegisterComplete from "./pages/auth/RegisterComplete";
+
 
 
 
