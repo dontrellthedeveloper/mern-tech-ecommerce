71fb24b08a4fb9b18713215a37e53a4ed456f7f4
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-01 20:59:19 -0400

    added user in redux state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -11,10 +11,38 @@ import RegisterComplete from "./pages/auth/RegisterComplete";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 
+import {auth} from './firebase';
+import {useDispatch} from "react-redux";
 
 
 
 const App = () => {
+    const dispatch = useDispatch();
+
+    // to check firebase auth state
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(async (user) => {
+            if(user) {
+                const idTokenResult = await user.getIdTokenResult();
+                console.log("User",user);
+
+                dispatch({
+                    type: 'LOGGED_IN_USER',
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token
+                    }
+                })
+            }
+        });
+
+        //cleanup
+
+        return () => unsubscribe();
+
+    }, []);
+
+
     return (
         <>
             <Header />
