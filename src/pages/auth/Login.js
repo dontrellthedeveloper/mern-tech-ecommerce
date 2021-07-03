5de06074ba9c2287c05f93d639f809171d1b5e1a
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-02 21:13:31 -0400

    added the ability to login with google

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,27 +1,66 @@
 import React, { useState } from "react";
-import { auth } from "../../firebase";
+import { auth, googleAuthProvider } from "../../firebase";
 import { toast } from "react-toastify";
 import { Button } from "antd";
-import { MailOutlined } from "@ant-design/icons";
+import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
 
-const Login = () => {
+const Login = ({ history }) => {
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState('');
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    let dispatch = useDispatch();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        setLoading(true);
+        // console.table(email, password);
         try {
-            const result = auth.signInWithEmailAndPassword(email, password);
-            console.log(result);
-        } catch (e) {
+            const result = await auth.signInWithEmailAndPassword(email, password);
+            // console.log(result);
+            const { user } = result;
+            const idTokenResult = await user.getIdTokenResult();
 
+            dispatch({
+                type: "LOGGED_IN_USER",
+                payload: {
+                    email: user.email,
+                    token: idTokenResult.token,
+                },
+            });
+            history.push("/");
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+            setLoading(false);
         }
+    };
+
+    const googleLogin = async () => {
+        auth
+            .signInWithPopup(googleAuthProvider)
+            .then(async (result) => {
+                const { user } = result;
+                const idTokenResult = await user.getIdTokenResult();
+                dispatch({
+                    type: "LOGGED_IN_USER",
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token,
+                    },
+                });
+                history.push("/");
+            })
+            .catch((err) => {
+                console.log(err);
+                toast.error(err.message);
+            });
     };
 
     const loginForm = () => (
         <form onSubmit={handleSubmit}>
-            <div className='form-group'>
+            <div className="form-group">
                 <input
                     type="email"
                     className="form-control"
@@ -32,10 +71,9 @@ const Login = () => {
                 />
             </div>
 
-
-            <div className='form-group'>
+            <div className="form-group">
                 <input
-                    type="email"
+                    type="password"
                     className="form-control"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
@@ -43,8 +81,7 @@ const Login = () => {
                 />
             </div>
 
-
-            <br/>
+            <br />
             <Button
                 onClick={handleSubmit}
                 type="primary"
@@ -64,9 +101,24 @@ const Login = () => {
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
-                    <h4>Login</h4>
-
+                    {loading ? (
+                        <h4 className="text-danger">Loading...</h4>
+                    ) : (
+                        <h4>Login</h4>
+                    )}
                     {loginForm()}
+
+                    <Button
+                        onClick={googleLogin}
+                        type="danger"
+                        className="mb-3"
+                        block
+                        shape="round"
+                        icon={<GoogleOutlined />}
+                        size="large"
+                    >
+                        Login with Google
+                    </Button>
                 </div>
             </div>
         </div>
