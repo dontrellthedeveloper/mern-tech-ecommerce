db8a0543aabed7a232162a3a468832293f4aab78
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-03 20:59:07 -0400

    added the ability to reset password if user forgets it

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Register from "./pages/auth/Register";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
+import ForgotPassword from "./pages/auth/ForgotPassword";
 
 import {auth} from './firebase';
 import {useDispatch} from "react-redux";
@@ -52,6 +53,7 @@ const App = () => {
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/register/complete" component={RegisterComplete} />
+                <Route exact path="/forgot/password" component={ForgotPassword} />
             </Switch>
         </>
     );
