dedcc8ae11cdd10afb58fda01760ae406ff6fa57
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-06 23:07:35 -0400

    added protected user route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import RegisterComplete from "./pages/auth/RegisterComplete";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 import ForgotPassword from "./pages/auth/ForgotPassword";
+import History from "./pages/user/History";
+import UserRoute from "./components/routes/UserRoute";
+
 
 import {auth} from './firebase';
 import {useDispatch} from "react-redux";
@@ -61,6 +64,7 @@ const App = () => {
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/register/complete" component={RegisterComplete} />
                 <Route exact path="/forgot/password" component={ForgotPassword} />
+                <UserRoute exact path="/user/history" component={History} />
             </Switch>
         </>
     );
