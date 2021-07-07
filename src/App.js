7fae13e8ef27791c6845bacfd0f89bf696e5d683
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-06 23:58:49 -0400

    added the ability for users to update their password

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import Header from "./components/nav/Header";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import History from "./pages/user/History";
 import UserRoute from "./components/routes/UserRoute";
+import Password from "./pages/user/Password";
+import Wishlist from "./pages/user/Wishlist";
 
 
 import {auth} from './firebase';
@@ -65,6 +67,8 @@ const App = () => {
                 <Route exact path="/register/complete" component={RegisterComplete} />
                 <Route exact path="/forgot/password" component={ForgotPassword} />
                 <UserRoute exact path="/user/history" component={History} />
+                <UserRoute exact path="/user/password" component={Password} />
+                <UserRoute exact path="/user/wishlist" component={Wishlist} />
             </Switch>
         </>
     );
