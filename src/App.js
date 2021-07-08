e3c2dc8c01d6409141f89d26113abde8ec1e6e20
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-07 23:19:36 -0400

    added the ability to update sub categories from the admin dashboard

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import AdminDashboard from "./pages/admin/AdminDashboard";
 import CategoryCreate from "./pages/admin/category/CategoryCreate";
 import CategoryUpdate from "./pages/admin/category/CategoryUpdate";
 import SubCreate from "./pages/admin/sub/SubCreate";
+import SubUpdate from "./pages/admin/sub/SubUpdate";
 
 import {auth} from './firebase';
 import {useDispatch} from "react-redux";
@@ -77,6 +78,7 @@ const App = () => {
                 <AdminRoute exact path="/admin/category" component={CategoryCreate} />
                 <AdminRoute exact path="/admin/category/:slug" component={CategoryUpdate}/>
                 <AdminRoute exact path="/admin/sub" component={SubCreate} />
+                <AdminRoute exact path="/admin/sub/:slug" component={SubUpdate} />
             </Switch>
         </>
     );
