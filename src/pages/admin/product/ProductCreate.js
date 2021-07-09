94f06f00cc364cf940e9126ebaa3d2c1480ef4d3
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-08 21:09:03 -0400

    added file upload component

--- src/pages/admin/product/ProductCreate.js
+++ src/pages/admin/product/ProductCreate.js
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import { createProduct } from "../../../functions/product";
 import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 import { getCategories, getCategorySubs } from "../../../functions/category";
+import FileUpload from "../../../components/forms/FileUpload";
 
 const initialState = {
     title: "Macbook Pro",
@@ -78,6 +79,10 @@ const ProductCreate = () => {
                 <div className="col-md-10">
                     <h4>Product create</h4>
                     <hr />
+
+                    <div className="p-3">
+                        <FileUpload />
+                    </div>
 
                     <ProductCreateForm
                         handleSubmit={handleSubmit}
