ba91d133412d5ba56d7de7a9279584dc4005ec1d
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-09 23:33:29 -0400

    added shop page

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import { Menu } from "antd";
 import {
@@ -7,6 +6,7 @@ import {
     UserOutlined,
     UserAddOutlined,
     LogoutOutlined,
+    ShoppingOutlined,
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import firebase from "firebase";
@@ -42,6 +42,10 @@ const Header = () => {
         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
             <Item key="home" icon={<AppstoreOutlined />}>
                 <Link to="/">Home</Link>
+            </Item>
+
+            <Item key="shop" icon={<ShoppingOutlined />}>
+                <Link to="/shop">Shop</Link>
             </Item>
 
             {!user && (
